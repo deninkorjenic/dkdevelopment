398772157a386bffd55c64ecd4d3c3bbb98446d1
Nedim Muhamedagić <muhamedagic.n@gmail.com>
2017-08-14 21:24:37 +0200

    fix: comment out the projector.js

--- web/app/themes/dk/resources/views/layouts/home.blade.php
+++ web/app/themes/dk/resources/views/layouts/home.blade.php
@@ -10,8 +10,8 @@
 
     @php(do_action('get_footer'))
     @php(wp_footer())
-    <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/three.min.js" />
-    <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/projector.js"></script>
+    <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/three.min.js"></script>
+    {{-- <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/projector.js"></script> --}}
     <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/canvas-renderer.js"></script>
     <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/3d-lines-animation.js"></script>
     <script src="{!! get_template_directory_uri() !!}/assets/scripts/util/bg/color.js"></script>
